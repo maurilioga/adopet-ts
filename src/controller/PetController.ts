518507ac5633514bca6dec9134187a4686802938
Maurilio Gonçalves Arantes <mauriliogoncarantes@gmail.com>
2025-12-23 22:07:01 -0300

    first commit

---
+++ src/controller/PetController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express";
+
+let listaPets = [];
+
+export default class PetController {
+    criarPet(req: Request, res: Response) {
+        const novoPet = req.body;
+        listaPets.push(novoPet);
+
+        return res.status(201).json(novoPet);
+    }
+}
